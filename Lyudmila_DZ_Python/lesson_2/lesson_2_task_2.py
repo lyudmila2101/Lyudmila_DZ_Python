0d056c09a4cc329450f73cea2e136654bba5a0f3
lyudmila2101 <lyudmila2101@mail.ru>
2026-02-24 14:40:26 +0300

    Добавлены изменения для урока 2

---
+++ Lyudmila_DZ_Python/lesson_2/lesson_2_task_2.py
@@ -0,0 +1,9 @@
+def is_year_leap(year):
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+    
+num = int(input("Введите год: "))
+result = is_year_leap(num)    
+print(f"год {num}: {result}")
---
+++ Lyudmila_DZ_Python/lesson_2/lesson_2_task_7.py
@@ -0,0 +1,3 @@
+lst = [18,14,10,6,2]
+lst = list(range(18, 1, -4))
+print(lst)
